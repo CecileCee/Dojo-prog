734cd63c4aa44f186dc93a2c799ad436bad3effa
CecileCee <cecile.muloway@gmail.com>
2016-04-12 11:21:47 +0200

    finished

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,1 +1,18 @@
+function HelloWorld () {
+  this.hello = function (name) {
+    // if (name === 'Bob' || name === 'Sally') {
+    //   return 'Hello, '+ name + '!';
+    // }
+    // return 'Hello, World!';
+    switch (name) {
+      case 'Bob':
+      case 'Sally':
+      return 'Hello, ' + name + '!';
+      default:
+      return 'Hello, World!';
+    }
+  }
+}
 
+
+module.exports = HelloWorld;
